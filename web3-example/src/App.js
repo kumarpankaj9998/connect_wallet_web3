2f2a9c8f1581fddabe882abcce930a50208ae356
Pankaj kumar <73705595+kumarpankaj9998@users.noreply.github.com>
2022-07-17 21:22:06 +0530

    commit2

---
+++ web3-example/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="py-2 mt-20 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover :bg-blue-800 ">
+        {" "}
+        Connect to metamask{" "}
+      </button>
+    </div>
+  );
+}
+
+export default App;
